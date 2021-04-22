d6931f34770e18eb984ff88343ac615b0b8eb51a
SergejViktorov <sergej.viktorov1310@gmail.com>
2021-04-22 14:37:41 +0300

    первая задача готовая

--- js/task-01.js
+++ js/task-01.js
@@ -1,17 +1,8 @@
-const calc
-const categories = document.querySelector('.item');
-const title = categories.querySelector('h2')
 
+const categories = document.querySelector('ul#categories');
 
+const categoriesItem = categories.querySelectorAll('li.item');
 
+categoriesItem.forEach(item => console.log('Категория:', item.firstElementChild.textContent) ||
+    console.log('Количество элементов:', item.lastElementChild.children.length))
 
-
-
-
-
-
-
-console.log('Категория:', title.textContent);
-
-const quantityElement = categoriesTitle.children;
-console.log('Количество элементов:', quantityElement.length)
