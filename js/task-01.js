56aab98ca5050fa81c4f5727ab96b22297b2a052
SergejViktorov <sergej.viktorov1310@gmail.com>
2021-04-22 13:25:40 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,17 @@
-const listTitle = document.querySelector('h2');
-console.log(listTitle);
+const calc
+const categories = document.querySelector('.item');
+const title = categories.querySelector('h2')
+
+
+
+
+
+
+
+
+
+
+console.log('Категория:', title.textContent);
+
+const quantityElement = categoriesTitle.children;
+console.log('Количество элементов:', quantityElement.length)
