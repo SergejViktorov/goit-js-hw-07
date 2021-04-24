653f6f4189528859c35f3d7d94984c03f8e38fc8
SergejViktorov <sergej.viktorov1310@gmail.com>
2021-04-25 00:33:52 +0300

    все готово кроме 8

--- js/task-06.js
+++ js/task-06.js
@@ -1,19 +1,13 @@
 const input = document.querySelector('#validation-input');
 
 input.addEventListener('blur', onInputBlur)
-input.addEventListener('input', onInput)
 input.classList.add('#validation-input')
 
-function onInput(event){
-    console.log('rt', Number(event.currentTarget.value.length))
-      }
-
 function onInputBlur(event) {
-    console.log('Инпут потерял фокус - событие blur');
-
-    event.currentTarget.value.length === input.dataset.length ? 
+`${event.currentTarget.value.length}` === input.dataset.length ? 
     event.currentTarget.classList.add('valid'): input.classList.add('invalid');
-        console.log(Number(event.currentTarget.value.length))
+
+        console.log(event.currentTarget.value.length)
         console.log(input.dataset.length);
         }
     
--- js/task-07.js
+++ js/task-07.js
@@ -1,9 +1,8 @@
 const input = document.querySelector('#font-size-control');
-console.log(input)
 const text = document.querySelector('#text');
-console.log(text)
 
 input.addEventListener('input', onInput);
+
 function onInput(event){
 text.style.fontSize = `${event.currentTarget.value}px`
 }
