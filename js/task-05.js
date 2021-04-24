02001ce51ab7093da4cf5f50809bd76af5cff4ad
SergejViktorov <sergej.viktorov1310@gmail.com>
2021-04-25 00:25:28 +0300

    5-ю добавил возрашение слова, 7-ю сделал, 6-я недоделал

--- js/task-05.js
+++ js/task-05.js
@@ -4,6 +4,9 @@ const nameEl = document.querySelector('#name-output')
 input.addEventListener('input', onInputChange)
 
 function onInputChange(event) {
-  nameEl.textContent = event.currentTarget.value
+  if(input.value === ''){
+    return nameEl.textContent = 'незнакомец';
+  }
+   nameEl.textContent = event.currentTarget.value
 }
 
--- js/task-06.js
+++ js/task-06.js
@@ -1,14 +1,24 @@
 const input = document.querySelector('#validation-input');
-input.addEventListener('focus', onInputFocus)
+
 input.addEventListener('blur', onInputBlur)
+input.addEventListener('input', onInput)
+input.classList.add('#validation-input')
 
+function onInput(event){
+    console.log('rt', Number(event.currentTarget.value.length))
+      }
 
-
-
-
-function onInputFocus (){
-    console.log('Инпут получил фокус - событие focus');
-};
-function onInputBlur() {
+function onInputBlur(event) {
     console.log('Инпут потерял фокус - событие blur');
-  }
+
+    event.currentTarget.value.length === input.dataset.length ? 
+    event.currentTarget.classList.add('valid'): input.classList.add('invalid');
+        console.log(Number(event.currentTarget.value.length))
+        console.log(input.dataset.length);
+        }
+    
+
+  
+ 
+  
+  
