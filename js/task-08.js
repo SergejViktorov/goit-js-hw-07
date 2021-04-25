5f52044374d61bdaf9bcd3ecb6f68c0c1d76632d
SergejViktorov <sergej.viktorov1310@gmail.com>
2021-04-25 22:47:04 +0300

    8-ю начал

--- js/task-08.js
+++ js/task-08.js
@@ -1,2 +1,26 @@
-const fdfeg = 67;
-console.log(fdfeg);
+const controlsEl = document.querySelector('#controls');
+const boxesEl = document.querySelector('#boxes');
+
+const inputEl = controlsEl.firstElementChild.addEventListener('input', onInputOut);
+console.dir(inputEl);
+
+const buttonRender = controlsEl.children[1];
+console.log(buttonRender);
+
+const buttonDestroy = controlsEl.children[2];
+console.log(buttonDestroy);
+
+function onInputOut(event) {
+  console.log(event.currentTarget.value);
+}
+
+// function createBoxes(amount);
+
+buttonDestroy.addEventListener('click', onClearOutput);
+function onClearOutput() {
+  controlsEl.firstElementChild = '';
+}
+
+console.log();
+console.log();
+console.log();
